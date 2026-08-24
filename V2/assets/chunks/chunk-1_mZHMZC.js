573b4887cfcda2a7cb5c5ad026e79ee7096d9146
Isaiah <isaiah.firestone@gmail.com>
2026-08-23 22:42:17 -0400

    Add v2

---
+++ V2/assets/chunks/chunk-1_mZHMZC.js
@@ -0,0 +1,1 @@
+import{a as e,q as s}from"./chunk-Dm6KcUHI.js";const m="div",n=e.forwardRef(({tag:t,...r},a)=>{const o=s(r)??t??m;return e.createElement(o,{...r,ref:a})});n.displayName="Text";export{n};
