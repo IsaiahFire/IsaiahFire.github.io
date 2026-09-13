dc1369805b5a9a8c47a9b47b60ed22119e768f5a
Isaiah <isaiah.firestone@gmail.com>
2026-09-13 15:50:26 -0400

    Create V2.3
    Update contact form sizing, add add'l link to video, rework VP message and brand, add mobile hamburger menu
    
    First push post-Cloudflare switch

---
+++ assets/chunks/chunk-BgSCtpE4.js
@@ -0,0 +1,1 @@
+import{r as e,o as s}from"./chunk-3JbMthdp.js";const m="div",n=e.forwardRef(({tag:o,...r},t)=>{const a=s(r)??o??m;return e.createElement(a,{...r,ref:t})});n.displayName="Text";export{n};
---
+++ assets/chunks/chunk-C5giwyIQ.js
@@ -0,0 +1,1 @@
+import{r as s,j as t,g as o}from"./chunk-3JbMthdp.js";const a=s.forwardRef((r,e)=>t.jsx(o,{...r,ref:e}));a.displayName="RichTextLink";export{a as e};
